039ef566e75f864815ced5316047af9e991590e9
sphillips04 <samariasharelle@gmail.com>
2022-06-02 14:33:45 -0500

    first commit

---
+++ pokeapi/src/main/java/com/example/pokeapi/api/pokapi.kt
@@ -0,0 +1,11 @@
+package com.example.pokeapi.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pokeapi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokapi)
+    }
+}
